344d7c0103f4157f02a5e7ce11cbdabee1e9d6ce
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-22 18:15:33 +0300

    new Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <img
+        src='logo512.png'
+        alt='logo'
+      />
+      <h2>The React Quiz</h2>
+    </header>
+  );
+}
