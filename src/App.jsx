38208d635ce33e74e671ea7381bc5a5857efafad
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-22 18:30:20 +0300

    adding the main app parent component to the app tree for render.This parent app component passes data from its nested components to the child component as chilren prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 function App() {
   return (
     <div className='app'>
       <Header />
+      <Main></Main>
     </div>
   );
 }
