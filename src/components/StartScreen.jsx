a152d771768ec8c004bde4e1c88f95e3eabaeee5
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-23 14:54:22 +0300

    new startscreen component

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,15 @@
+import { useQuestions } from "../context/QuestionsContext";
+
+function StartScreen() {
+  const { questions } = useQuestions();
+
+  return (
+    <div className='start'>
+      <h2>Welcome to The React Quiz!</h2>
+      <h3>{questions.length} questions to test your React mastery</h3>
+      <button> start</button>
+    </div>
+  );
+}
+
+export default StartScreen;
