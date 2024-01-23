e18911f034789ae1da206d801d72b9853d432f05
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-23 14:57:03 +0300

    provided context to the entire application tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+import StartScreen from "./components/StartScreen";
 function App() {
   return (
     <div className='app'>
       <Header />
-      <Main></Main>
+      <Main>
+        <StartScreen />
+      </Main>
     </div>
   );
 }
