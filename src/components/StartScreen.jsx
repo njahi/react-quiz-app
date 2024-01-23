775d297fb909bff82b667b15536d57511af65178
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-23 15:24:15 +0300

    styles to the h2 h3 and button elements in the startscreen component

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -7,7 +7,7 @@ function StartScreen() {
     <div className='start'>
       <h2>Welcome to The React Quiz!</h2>
       <h3>{questions.length} questions to test your React mastery</h3>
-      <button> start</button>
+      <button className='btn btn-ui'> start</button>
     </div>
   );
 }
