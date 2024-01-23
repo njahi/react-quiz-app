ad6eecfecc476e641998534d6fbdfe3cef7f6778
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-23 15:38:04 +0300

    rendered the nested startscreen component conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import StartScreen from "./components/StartScreen";
+import { useQuestions } from "./context/QuestionsContext";
 function App() {
+  const { isLoading } = useQuestions();
   return (
     <div className='app'>
       <Header />
-      <Main>
-        <StartScreen />
-      </Main>
+      <Main>{isLoading === false && <StartScreen />}</Main>
     </div>
   );
 }
