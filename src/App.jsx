1c6e6525338af57052df2bc1b40d491c91802f8c
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-23 15:58:23 +0300

    added the Error component dependant on the error state and also added some style to the Error component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Main from "./components/Main";
 import Loader from "./components/Loader";
 import StartScreen from "./components/StartScreen";
 import { useQuestions } from "./context/QuestionsContext";
+import Error from "./components/Error";
 function App() {
-  const { isLoading } = useQuestions();
+  const { isLoading, error } = useQuestions();
   return (
     <div className='app'>
       <Header />
       {isLoading === true && <Loader />}
+      {error && <Error />}
       <Main>{isLoading === false && <StartScreen />}</Main>
     </div>
   );
