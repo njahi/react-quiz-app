fded71a5a439e73759c1cfb61159e3e9ad9f092b
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-29 11:16:21 +0300

    used the onClick function in the Startscren component to give the button elememnt some functionality once clicked.the functionality comes from the onclick function in the context api

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,13 +1,17 @@
 import { useQuestions } from "../context/QuestionsContext";
 
 function StartScreen() {
-  const { questions } = useQuestions();
+  const { questions, onClick } = useQuestions();
 
   return (
     <div className='start'>
       <h2>Welcome to The React Quiz!</h2>
       <h3>{questions.length} questions to test your React mastery</h3>
-      <button className='btn btn-ui'>lets start</button>
+      <button
+        onClick={onClick}
+        className='btn btn-ui'>
+        lets start
+      </button>
     </div>
   );
 }
