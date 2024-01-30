7f8a2f419ba10af5f3f6c09eb14c9e487beebbc5
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-30 12:22:02 +0300

    in the context api i replaced managing state with the useState hook and made use of the usereducer hooks that makes numerous state management easy

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Loader from "./components/Loader";
 import StartScreen from "./components/StartScreen";
-import { useQuestions } from "./context/QuestionsContext";
+import { useQuestions } from "./context/QuestionsContextReducer";
 import Error from "./components/Error";
 import Question from "./components/Question";
 import Progress from "./components/Progress";
 function App() {
-  const { isLoading, error, active } = useQuestions();
+  const { status } = useQuestions();
   return (
     <div className='app'>
       <Header />
-      {isLoading === true && <Loader />}
-      {error && <Error />}
       <Main>
-        {isLoading === false && !error ? <StartScreen /> : ""}
-        {active === true && (
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen />}
+        {status === "active" && (
           <>
             <Progress />
             <Question />
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,14 +1,14 @@
 import { useQuestions } from "../context/QuestionsContext";
 
 function StartScreen() {
-  const { questions, onClick } = useQuestions();
+  const { questions, dispatch } = useQuestions();
 
   return (
     <div className='start'>
       <h2>Welcome to The React Quiz!</h2>
       <h3>{questions.length} questions to test your React mastery</h3>
       <button
-        onClick={onClick}
+        onClick={() => dispatch({ type: "start" })}
         className='btn btn-ui'>
         lets start
       </button>
