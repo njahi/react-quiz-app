6a30b26bb131ddc50efe40e08ef40e8c72de3396
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-30 12:24:10 +0300

    replacing questions.length with the numQuestions value calculated from the context api

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,12 +1,12 @@
 import { useQuestions } from "../context/QuestionsContext";
 
 function StartScreen() {
-  const { questions, dispatch } = useQuestions();
+  const { numQuestions, dispatch } = useQuestions();
 
   return (
     <div className='start'>
       <h2>Welcome to The React Quiz!</h2>
-      <h3>{questions.length} questions to test your React mastery</h3>
+      <h3>{numQuestions} questions to test your React mastery</h3>
       <button
         onClick={() => dispatch({ type: "start" })}
         className='btn btn-ui'>
