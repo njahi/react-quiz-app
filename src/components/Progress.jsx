200b76e3ef99411f65e6ef254b85b683dbc2c23a
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-30 12:04:34 +0300

    Progress bar component code fixing

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -2,9 +2,13 @@ import { useQuestions } from "../context/QuestionsContext";
 
 export default function Progress() {
   const { questions } = useQuestions();
+  const index = questions.index;
   return (
     <header className='progress'>
-      <progress />
+      <progress
+        max={questions.length}
+        value={index + Number(questions.answer !== null)}
+      />
       <p>
         Question<strong>{}</strong>
       </p>{" "}
