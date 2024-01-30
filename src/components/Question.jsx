f54df458f19c6fe39a4b261a93c25d2c8e7e3bb4
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-30 13:07:21 +0300

    code adjustment in the question component.In that, we use the index value to get the question as one and not map all the questions at once but a question at a time interms of its index in the questions array

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,18 +2,12 @@ import { useQuestions } from "../context/QuestionsContextReducer";
 import Options from "./Options";
 
 export default function Question() {
-  const { questions } = useQuestions();
+  const { questions, index } = useQuestions();
+  const question = questions.at(index);
   return (
     <div>
-      {questions.map((qtn) => (
-        <div key={qtn.id}>
-          <h4>{qtn.question}</h4>
-          <Options
-            question={qtn}
-            id={qtn.id}
-          />
-        </div>
-      ))}
+      <h4>{question.question}</h4>
+      <Options question={question} />
     </div>
   );
 }
