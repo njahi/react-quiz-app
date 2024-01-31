5f797afff7ec7a5b84900910556ff2796bfb354e
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-31 15:04:25 +0300

    added a dispatch to every buttton to know what to do depending on the type attached to the onclick property

--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -4,6 +4,19 @@ export default function NextButton() {
   const { dispatch, answer, index, numQuestions } = useQuestions();
   if (answer === null) return null;
   if (index < numQuestions - 1)
-    return <button className='btn btn-ui'>Next</button>;
-  if (index === numQuestions - 1) return <button>Finish</button>;
+    return (
+      <button
+        className='btn btn-ui'
+        onClick={() => dispatch({ type: "nextQuestion" })}>
+        Next
+      </button>
+    );
+  if (index === numQuestions - 1)
+    return (
+      <button
+        className='btn btn-ui'
+        onClick={() => dispatch({ type: "finish" })}>
+        Finish
+      </button>
+    );
 }
