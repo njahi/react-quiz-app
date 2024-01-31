e63cbda93cc7eb21883e3f95533cb520911d0a2e
Humphrey Gatinu <njahigatinu@gmail.com>
2024-01-31 15:00:35 +0300

    adding some logic and conditional rendering of the NextButton component

--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -1,3 +1,9 @@
+import { useQuestions } from "../context/QuestionsContextReducer";
+
 export default function NextButton() {
-  return <button>Next</button>;
+  const { dispatch, answer, index, numQuestions } = useQuestions();
+  if (answer === null) return null;
+  if (index < numQuestions - 1)
+    return <button className='btn btn-ui'>Next</button>;
+  if (index === numQuestions - 1) return <button>Finish</button>;
 }
