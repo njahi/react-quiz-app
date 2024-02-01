e79b0a07266bbc8480a4be4bfc3efe14a646d3f5
Humphrey Gatinu <njahigatinu@gmail.com>
2024-02-01 13:33:35 +0300

    more logic in the finish screen component

--- src/components/FinishScreen.jsx
+++ src/components/FinishScreen.jsx
@@ -1,7 +1,14 @@
+/* eslint-disable no-unused-vars */
 import { useQuestions } from "../context/QuestionsContextReducer";
 
 export default function FinishScreen() {
   const { points, maxPossiblePoints, highscore, dispatch } = useQuestions();
   const percentage = (points / maxPossiblePoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "🙃";
+  if (percentage >= 0 && percentage < 50) emoji = "🤨";
+  if (percentage === 0) emoji = "🤦‍♂️";
   return null;
 }
