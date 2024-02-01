a86d61459243397624d50e252965c38337026189
Humphrey Gatinu <njahigatinu@gmail.com>
2024-02-01 13:57:59 +0300

    added some logic to the component

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,5 +1,26 @@
-import React from "react";
-
+import { useEffect } from "react";
+import { useQuestions } from "../context/QuestionsContextReducer";
 export default function Timer() {
-  return <div>Timer</div>;
+  const { dispatch, secondsRemaining } = useQuestions();
+
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+
+      return () => clearInterval(id);
+    },
+    [dispatch]
+  );
+  return (
+    <div className='timer'>
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
 }
