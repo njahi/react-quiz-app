51981a91fe3bfa725f67f512b9b29f9f6b8079fb
Humphrey Gatinu <njahigatinu@gmail.com>
2024-02-01 13:35:02 +0300

    added the return block statement jsx

--- src/components/FinishScreen.jsx
+++ src/components/FinishScreen.jsx
@@ -10,5 +10,18 @@ export default function FinishScreen() {
   if (percentage >= 50 && percentage < 80) emoji = "🙃";
   if (percentage >= 0 && percentage < 50) emoji = "🤨";
   if (percentage === 0) emoji = "🤦‍♂️";
-  return null;
+  return (
+    <>
+      <p className='result'>
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className='highscore'>(Highscore: {highscore} points)</p>
+      <button
+        className='btn btn-ui'
+        onClick={() => dispatch({ type: "restart" })}>
+        Restart quiz
+      </button>
+    </>
+  );
 }
